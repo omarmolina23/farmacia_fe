007f7c81ee918a2d78fa7dde6f060ca46e8cc5ba
saimers <saimeradriansr@ufps.edu.co>
2025-03-22 17:11:03 -0500

    feat: add Login component and update global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './pages/login'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <Login />
   )
 }
 
