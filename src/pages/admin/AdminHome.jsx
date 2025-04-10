50d2b521b3fc772fc003028de78beea2bb513256
saimersaavedra <saimeradriansr@ufps.edu.co>
2025-04-10 12:12:31 -0500

    feat: install shadcn, configure jsconfig, and add initial setup

--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -1,10 +1,10 @@
 import AdminLayout from "../../modules/admin/layouts/AdminLayout";
 import PageHeader from "../../components/PageHeader";
+import { Button } from "@/components/ui/button"
 
 export default function AdminHome() {
     return (
         <AdminLayout>
-            <PageHeader title="Categorias" />
             <div className="w-full p-4 flex justify-between items-center">
             </div>
         </AdminLayout>
