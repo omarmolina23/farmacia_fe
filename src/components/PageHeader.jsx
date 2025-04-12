394c2c10f79ef21e9b378d09b3a5730da6a0173e
saimersaavedra <saimeradriansr@ufps.edu.co>
2025-04-12 00:46:33 -0500

    refactor(sidebar): restructure sidebar component while preserving design

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Separator } from "../components/ui/separator"
+import { SidebarTrigger } from "../components/ui/sidebar"
 
 const PageHeader = ({ title }) => {
   return (
-    <div className="w-full bg-[#D0F25E] p-3 flex justify-between items-center">
-      <h2 className="w-full text-lg md:text-xl text-left pl-4 font-semibold">{title}</h2>
-    </div>
+    <header className="w-full bg-[#D0F25E] p-3 flex justify-between items-center">
+      <div className="flex w-full items-center gap-1 lg:gap-2">
+        <SidebarTrigger />
+        <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4 bg-black" />
+        <h1 className="text-base font-bold">{title}</h1>
+      </div>
+    </header>
   );
 };
 
