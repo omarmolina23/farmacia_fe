a34263f7c6c30e56595c08a410fd50f529780a59
Omar <omardavidjm@ufps.edu.co>
2025-04-17 11:45:34 -0500

    feat: implement product management features including listing, editing, and status toggling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ type = "button", title, onClick, color, textColor = "text-whit
     <button
       type={type}
       onClick={onClick}
-      className={`${color} ${textColor} font-semibold py-2 px-6 sm:px-8 md:px-10 lg:px-12 rounded-lg text-sm sm:text-base md:text-lg transition-all duration-300 ease-in-out hover:bg-opacity-80 focus:ring-2 focus:ring-gray-300 shadow-md active:scale-95`}
+      className={`${color} ${textColor} font-semibold py-1.5 px-4 sm:px-8 md:px-10 lg:px-12 rounded-lg text-sm sm:text-base md:text-lg transition-all duration-300 ease-in-out hover:bg-opacity-80 focus:ring-2 focus:ring-gray-300 shadow-md active:scale-95`}
       aria-label={title}
     >
       {title}
