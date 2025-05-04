03bc0e0d7a4cefea32fdb32b9e3c9a6afcabb165
Omar <omardavidjm@ufps.edu.co>
2025-05-04 15:26:48 -0500

    refactor: remove response interceptor for token refresh handling in axios instance

--- src/config/axios.js
+++ src/config/axios.js
@@ -23,26 +23,5 @@ instance.interceptors.request.use(
   }
 );
 
-instance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const { token: newToken } = await refreshTokenService();
-        localStorage.setItem("token", newToken);
-        originalRequest.headers.Authorization = `Bearer ${newToken}`;
-        return instance(originalRequest);
-      } catch (refreshError) {
-        toast.error("Sesión expirada, por favor inicie sesión nuevamente");
-        signOutService();
-        window.location.href = "/";
-        return Promise.reject(refreshError);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default instance;
