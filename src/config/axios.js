39bd03880370a988408051d2acb5dd0daff05d9d
Omar <omardavidjm@ufps.edu.co>
2025-05-29 15:14:11 -0500

    feat: Add response interceptor to handle 401 errors and token refresh logic

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,6 +5,20 @@ const instance = axios.create({
   withCredentials: true,
 });
 
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+}
+
 instance.interceptors.request.use(
   async (config) => {
     let token = localStorage.getItem("token");
@@ -16,6 +30,52 @@ instance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Interceptor de respuesta: maneja el 401
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({
+            resolve: (token) => {
+              originalRequest.headers.Authorization = `Bearer ${token}`;
+              resolve(instance(originalRequest));
+            },
+            reject: (err) => reject(err),
+          });
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try {
+        const res = await axios.post(
+          `${import.meta.env.VITE_BACKEND_URL}/auth/refresh-token`,
+          {},
+          { withCredentials: true }
+        );
+        const newToken = res.data.token;
+        localStorage.setItem("token", newToken);
+        instance.defaults.headers.Authorization = `Bearer ${newToken}`;
+        processQueue(null, newToken);
+        return instance(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        window.location.href = "/";
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
     return Promise.reject(error);
   }
 );
