3a68d327da4b7dd1fd9146c1b2d387b9f2443bf6
Omar <omardavidjm@ufps.edu.co>
2025-06-08 20:34:39 -0500

    feat: add onWheel event to TextField and ProductForm to prevent zooming on mobile devices

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TextField = ({ type, id, name, onChange, value, placeholder, disabled }) => {
+const TextField = ({ type, id, name, onChange, onWheel, value, placeholder, disabled }) => {
   return (
     <input
       className="p-3 rounded-md bg-gray-100 w-full focus:outline-none focus:ring-2 focus:ring-green-500"
@@ -9,6 +9,7 @@ const TextField = ({ type, id, name, onChange, value, placeholder, disabled }) =
       name={name} 
       value={value}
       onChange={onChange}
+      onWheel={onWheel} // Prevents zooming on mobile devices
       placeholder={placeholder}
       disabled={disabled}
     />
