1a19e227c9e293ecb75de14973b992f69bd767cf
saimersaavedra <127253405+saimeers@users.noreply.github.com>
2025-11-19 23:06:44 -0500

    Update generateInvoiceData.js

--- src/modules/admin/sales/components/invoice/electronic_invoice/generateInvoiceData.js
+++ src/modules/admin/sales/components/invoice/electronic_invoice/generateInvoiceData.js
@@ -41,7 +41,7 @@ export function buildInvoiceData({ fecha, ventaId, cliente, productos }) {
 
     return {
         numbering_range_id: 9,
-        reference_code: `fact${idVenta}`,
+        reference_code: `FAC${idVenta}`,
         observation: "",
         payment_form: "1",
         payment_method_code: "10",
